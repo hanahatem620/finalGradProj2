fe9adf258ddeb7970a5875ad62ee4737ea152d96
Hana Hatem <hanahatem.620@gmail.com>
2026-06-14 09:26:44 +0300

    last

--- GlamNgo Front End/my-app/src/app/payment-success/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import SuccessPaymentThird from '../_components/BookingSteps/SuccessPaymentThird'
-
-export default function PaymentSuccess() {
-  return (
-    <SuccessPaymentThird/>
-  )
-}
